58d8838ef19ec6b70a5a19b66a29fbbd6a4600df
abhishek.goyal2 <abhishek.goyal2@DEL1-DHP-27956.synapse.com>
2017-07-28 13:06:20 +0530

    test

--- src/aS05_Java/Comparestrings.java
+++ src/aS05_Java/Comparestrings.java
@@ -5,7 +5,7 @@ public class Comparestrings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s1="Sachin Tendulkar";  
-		   String s2="Sachin";  
+		   String s2="Sachinten";  
 		   
 		   System.out.println(s1.compareTo(s2));
 		   System.out.println(s2.compareTo(s1));
