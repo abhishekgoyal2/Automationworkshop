e88058c6bda4678f48ed6537d36e04da9d4ac205
abhishek.goyal2 <abhi25goel@gmail.com>
2017-08-03 18:20:51 +0530

    resource file

--- src/AS06_Automation/yahoo_verifyphone.java
+++ src/AS06_Automation/yahoo_verifyphone.java
@@ -1,9 +1,28 @@
 package AS06_Automation;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import CoreJavaASO4.Loadproperty;
+
 public class yahoo_verifyphone {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver =new FirefoxDriver();
+		
+			String Appurl =Loadproperty.getvalue("url");
+		
+		
+driver.get(Appurl);
+driver.manage().window().maximize();
+driver.findElement(By.xpath(".//*[@id='uh-signin']")).click();
+driver.findElement(By.xpath(".//*[@id='createacc']")).click();
+	    
 
 	}
 
--- src/CoreJavaASO4/quickrTC2.java
+++ src/CoreJavaASO4/quickrTC2.java
@@ -3,7 +3,7 @@ package CoreJavaASO4;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
