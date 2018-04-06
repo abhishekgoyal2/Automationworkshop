085f2880f9b572b7e4e8e1087aa6207a7876b703
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-06 16:51:35 +0530

    commit radio button

---
+++ src/seleniumPrograms/radiobuttons.java
@@ -0,0 +1,29 @@
+package seleniumPrograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class radiobuttons {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		String exePath = "C:/Projects/chromedriver_win32 (1)/chromedriver.exe";
+		
+		System.setProperty("webdriver.chrome.driver", exePath);
+			
+		driver =new ChromeDriver();
+			driver.get("http://www.echoecho.com/htmlforms10.htm");
+			Thread.sleep(1000);
+			driver.manage().window().maximize();
+			
+			int count =driver.findElements(By.xpath("//input[@name='group1']")).size();
+			Thread.sleep(2500);
+			
+			System.out.println(count);
+			System.out.println("test pass");
+
+	}
+
+}
