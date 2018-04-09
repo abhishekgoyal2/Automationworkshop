ccbcc04fbe95eb77dbd6bb3492f163072611258f
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-09 11:21:41 +0530

    test if text

--- src/seleniumPrograms/radiobuttons.java
+++ src/seleniumPrograms/radiobuttons.java
@@ -21,7 +21,24 @@ public class radiobuttons {
 			Thread.sleep(2500);
 			
 			System.out.println(count);
-			System.out.println("test pass");
+			for (int i=0;i<count;i++){
+				driver.findElements(By.xpath("//input[@name='group1']")).get(i).click();
+				Thread.sleep(1500);
+				String text =driver.findElements(By.xpath("//input[@name='group1']")).get(i).getAttribute("value");
+				System.out.println(text);
+//				System.out.println(driver.findElements(By.xpath("//input[@name='group1']")).get(i).getAttribute("value"));
+			
+				if (text =="Cheese")
+				{
+					driver.findElements(By.xpath("//input[@name='group1']")).get(i).click();
+					
+				}
+				Thread.sleep(2000);
+			}
+			
+				System.out.println("test pass");
+				
+				driver.quit();
 
 	}
 
