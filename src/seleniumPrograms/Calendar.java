d1cac83615eb75deb376dfd69c98d6e2f0767644
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-10 17:07:14 +0530

    commit calendar

--- src/seleniumPrograms/Calendar.java
+++ src/seleniumPrograms/Calendar.java
@@ -1,15 +1,17 @@
 package seleniumPrograms;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver ;
 		String exePath = "C:/Projects/chromedriver_win32 (1)/chromedriver.exe";
@@ -18,34 +20,33 @@ public class Calendar {
 		driver =new ChromeDriver();
 		
 		driver.get("https://www.path2usa.com/travel-companions");
+		
 		driver.manage().window().maximize();
 		
-		//April 14
-			
-		driver.findElement(By.xpath("//*[@id='travel_date']")).click();
-		
-		driver.findElement(By.cssSelector("[class='datepicker-days'][class='datepicker-switch']")).getText().contains("April");
-		
-		while(driver.findElement(By.cssSelector("[class='datepicker-days'][class='datepicker-switch']")).getText().contains("April")) 
+		//April 23
+		driver.findElement(By.xpath(".//*[@id='travel_date']")).click();
+
+
+		while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("May"))
 		{
-			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
 		}
-		
-		List<WebElement> Calendarlist=driver.findElements(By.className("day"));
-		
-	int count =driver.findElements(By.className("day")).size();
-	
-	for (int i=0;i<count;i++)
-	{
-		String text =driver.findElements(By.className("day")).get(i).getText();
-	
-		if(text.equalsIgnoreCase("10"))
+
+
+		List<WebElement> dates= driver.findElements(By.className("day"));
+		//Grab common attribute//Put into list and iterate
+		int count=driver.findElements(By.className("day")).size();
+
+		for(int i=0;i<count;i++)
 		{
-			driver.findElements(By.className("day")).get(i).click();
-			break;
+		String text=driver.findElements(By.className("day")).get(i).getText();
+		if(text.equalsIgnoreCase("21"))
+		{
+		driver.findElements(By.className("day")).get(i).click();
+		break;
 		}
 	}
 
 	driver.quit();
-}
+	}
 }
