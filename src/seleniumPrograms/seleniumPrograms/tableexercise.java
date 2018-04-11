e52fdd4bf8659b5b9fa748d23329bd044affcd27
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-11 17:50:17 +0530

    side

--- src/seleniumPrograms/seleniumPrograms/tableexercise.java
+++ src/seleniumPrograms/seleniumPrograms/tableexercise.java
@@ -22,13 +22,30 @@ public class tableexercise {
 			WebElement table =driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
 			int rowcount =table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms']")).size();
 			int count =table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
-//			int score=0, total;
+		int sum=0;
 			for (int i=0;i<count-2;i++)
 			{
-			 System.out.println(table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).get(i).getText());
+				String score=table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).get(i).getText();
+			 int scoreint=Integer.parseInt(score);
+			 sum =sum+scoreint;
+				System.out.println(table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).get(i).getText());
+				System.out.println(sum);
 				
 	}
+			String extras=driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText();
 			System.out.println(driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText());
-
+			
+			int extraint=Integer.parseInt(extras);
+			sum =sum+extraint;
+			System.out.println(sum);
+			String total=driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText();
+			System.out.println(driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText());
+			int totalint=Integer.parseInt(total);
+			
+			if(sum==totalint)
+			{
+				System.out.println("total match sum");
+			}
+		
 }
 }
