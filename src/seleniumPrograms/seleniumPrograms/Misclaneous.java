d45077e3a175c467dcf5be2a0e784e215046da54
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-11 12:13:39 +0530

    test commit checkbox

--- src/seleniumPrograms/seleniumPrograms/Misclaneous.java
+++ src/seleniumPrograms/seleniumPrograms/Misclaneous.java
@@ -6,25 +6,50 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Misclaneous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 String exePath = "C:/Projects/chromedriver_win32 (1)/chromedriver.exe";
 WebDriver driver;
 
 		System.setProperty("webdriver.chrome.driver", exePath);
 		driver =new ChromeDriver();
-		driver =new ChromeDriver();
+	
 		driver.get("https://www.dailyfantasycricket.com/register");
 		driver.manage().window().maximize();
+//		Thread.sleep(1000);
+//		driver.switchTo().alert().dismiss();;
+		int num=findFramenumber(driver,By.xpath("//*[@id='recaptcha-anchor']/div[5]"));
+		driver.switchTo().frame(num);
+		driver.findElement(By.xpath("/*[@id='recaptcha-anchor']/div[5]")).click();
+		driver.switchTo().defaultContent();
+
+	
 	}
 
-	public void switchtoFrame(WebDriver driver)
+	public static int findFramenumber(WebDriver driver,By by)
 	{
+		int i;
 		int framecount=driver.findElements(By.tagName("iframe ")).size();
-		for (int i=0;i<framecount;i++)
+		for (i=0;i<framecount;i++)
 		{
 			driver.switchTo().frame(i);
-			driver.findElement(By.xpath("//*[@id='recaptcha-anchor']/div[5]"));
+			int count =driver.findElements(by).size();
+			if(count>0)
+			{
+//				driver.findElement(By.xpath("/*[@id='recaptcha-anchor']/div[5]")).click();
+				break ;
 		}
+			
+			else
+			{
+				System.out.println("contiue script");
 	}
+			
+			
+			
+}
+	driver.switchTo().defaultContent();
+		
+		return i ;
+}
 }
