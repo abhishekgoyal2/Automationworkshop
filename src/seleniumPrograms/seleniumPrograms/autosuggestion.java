2e6a382233d79c42add198d105792ae04bb841aa
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-11 15:15:57 +0530

    suggestion

--- src/seleniumPrograms/seleniumPrograms/autosuggestion.java
+++ src/seleniumPrograms/seleniumPrograms/autosuggestion.java
@@ -1,5 +1,7 @@
 package seleniumPrograms.seleniumPrograms;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -8,9 +10,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class autosuggestion {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver;
+		int i=0;
 		String exePath = "C:/Projects/chromedriver_win32 (1)/chromedriver.exe";
 		
 		System.setProperty("webdriver.chrome.driver", exePath);
@@ -18,18 +21,46 @@ public class autosuggestion {
 		driver =new ChromeDriver();
 			driver.get("https://ksrtc.in/oprs-web/");
 			driver.manage().window().maximize();
+//			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+			
 			driver.findElement(By.xpath("//input[@id='fromPlaceName']")).sendKeys("Beng");
-			driver.findElement(By.xpath("//input[@id='fromPlaceName']")).sendKeys(Keys.DOWN);
+			
+			Thread.sleep(1000);
 			driver.findElement(By.xpath("//input[@id='fromPlaceName']")).sendKeys(Keys.DOWN);
 			
 			JavascriptExecutor js =  (JavascriptExecutor)driver;
 			
-			String script ="return document.getElementById(\"fromPlaceName\").value;" ;
+			String script ="return document.getElementById(\"fromPlaceName\").value" ;
 			
 			String text =(String) js.executeScript(script);
 			
-			System.out.println(text);
-			
-	}
+			while(!text.equalsIgnoreCase("BENGALURU INTERNATION AIPORT"))
+			{
+				
+				i++;
+				driver.findElement(By.xpath("//input[@id='fromPlaceName']")).sendKeys(Keys.DOWN);
+				text =(String) js.executeScript(script);
+				System.out.println(text);
+				if(i>10)
+				{
+				break;
+				}
 
+				}
+
+				if(i>10)
+				{
+				System.out.println("Element not found");
+				}
+				else
+				{
+				System.out.println("Element  found");
+				}
+			
+				driver.quit();	
+	}
+			
+						
 }
+
+
